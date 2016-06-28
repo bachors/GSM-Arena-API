739fc0a5a3b3911c76f9b0746d6149902a6acfb7
Ican Bachors <bachor.can@gmail.com>
2016-06-28 23:22:07 +0700

    Create API JSON

---
+++ api/index.php
@@ -0,0 +1,30 @@
+<?php
+
+require("../lib/gsm.php");
+
+// Create objek
+$gsm = new gsm();
+
+// Return ARRAY
+if(!empty($_GET['query'])){
+	$data = $gsm->search($_GET['query']);
+}else if(!empty($_GET['slug'])){
+	$data = $gsm->detail($_GET['slug']);
+}else{
+	$data = array(
+		"status" => "error"
+	);
+}
+
+// JSON
+header('Content-Type: application/json');
+
+// Tanda * berarti memberi hak akses kesemua host/domain untuk mengkonsusmi data JSON ini via AJAX.
+// Jika sobat hanya ingin domain sobat saja yang bisa mengkonsusmi data JSON ini via AJAX tinggal rubah seperti ini:
+// header('Access-Control-Allow-Origin: http://domain-sobat.com');
+header('Access-Control-Allow-Origin: *');
+
+// Convert ARRAY to JSON
+echo json_encode($data, JSON_PRETTY_PRINT);
+
+?>
