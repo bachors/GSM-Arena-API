521cd6d991f026c9ab2620c5c66aa3a881ed4ace
Andrian Iliev <fulgerx2007@gmail.com>
2020-01-12 00:02:42 +0200

    Minor fixes

--- lib/gsm.php
+++ lib/gsm.php
@@ -1,19 +1,19 @@
 <?php
 
 /*********************************************************************
-PHP Class untuk grab data di website gsm arena
-menggunakan cURL dan simple html dom.
-
-* Coded by Ican Bachors 2016.
-* http://ibacor.com/labs/gsm-arena-api
-* Updates will be posted to this site.
-*********************************************************************/
+ * PHP Class untuk grab data di website gsm arena
+ * menggunakan cURL dan simple html dom.
+ * Coded by Ican Bachors 2016.
+ * http://ibacor.com/labs/gsm-arena-api
+ * Updates will be posted to this site.
+ *********************************************************************/
 
 error_reporting(0);
 
 class Gsm
 {
     public $base_url = 'https://www.gsmarena.com/';
+
     public function __construct()
     {
         // Include library simple html dom
@@ -23,10 +23,10 @@ class Gsm
         error_reporting(E_ALL);
 
         // Fix bug slug symbol
-        $this->simbol = array("&", "+");
-        $this->kata = array("_and_", "_plus_");
+        $this->simbol = ["&", "+"];
+        $this->kata = ["_and_", "_plus_"];
     }
-       
+
     ####################### NGE cURL ##########################
     private function mycurl($url)
     {
@@ -37,28 +37,25 @@ class Gsm
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_URL, $url);
-        
-        // Gagal ngecURL
+
         if (!$site = curl_exec($ch)) {
             return 'offline';
-        }
-        
-        // Sukses ngecURL
-        else {
+        } else {
             return $site;
         }
     }
+
     ####################### END cURL ##########################
     public function getBrands()
     {
         $result = array();
-        $url  = 'https://www.gsmarena.com/makers.php3';
+        $url = 'https://www.gsmarena.com/makers.php3';
         $ngecurl = $this->mycurl($url);
         if ($ngecurl == 'offline') {
             $result["status"] = "error";
             $result["data"] = array();
         } else {
-            $html  = str_get_html($ngecurl);
+            $html = str_get_html($ngecurl);
             $div = $html->find('div[class=st-text] table tbody', 0);
             if ($div->find('tr', 0)) {
                 $result["status"] = "success";
@@ -84,6 +81,7 @@ class Gsm
         }
         return $result;
     }
+
     public function getNavigators($url)
     {
         $result = array();
@@ -92,12 +90,12 @@ class Gsm
             $result["status"] = "error";
             $result["data"] = array();
         } else {
-            $html  = str_get_html($ngecurl);
+            $html = str_get_html($ngecurl);
             $div = $html->find('div[class=nav-pages]', 0);
             if ($div && $div->find('a', 0)) {
                 $result["status"] = "success";
                 foreach ($div->find('a') as $tr) {
-                    $href = $this->base_url.$tr->href;
+                    $href = $this->base_url . $tr->href;
                     $result["data"][] = array(
                         "href" => $href,
                     );
@@ -109,6 +107,7 @@ class Gsm
         }
         return $result;
     }
+
     public function getProducts($pages)
     {
         $products = array(
@@ -122,7 +121,7 @@ class Gsm
                 $result["status"] = "error";
                 $result["data"] = array();
             } else {
-                $html  = str_get_html($ngecurl);
+                $html = str_get_html($ngecurl);
                 $div = $html->find('div[class=makers]', 0);
                 if ($div->find('li', 0)) {
                     $result["status"] = "success";
@@ -142,6 +141,7 @@ class Gsm
         }
         return $products;
     }
+
     public function search($q = "")
     {
         $result = array();
@@ -150,23 +150,23 @@ class Gsm
         $nameArray = array_map('strtolower', $nameArray);
         $key = array_search(strtolower($q), $nameArray);
         if ($key) {
-            $url = $this->base_url.$brands['data'][$key]['href'];
+            $url = $this->base_url . $brands['data'][$key]['href'];
             $pages = $this->getNavigators($url);
             $pages = array_column($pages['data'], 'href');
             array_unshift($pages, $url);
             $products = $this->getProducts($pages);
             return $products;
         } else {
-            $url  = 'https://www.gsmarena.com/results.php3?sQuickSearch=yes&sName='.urlencode($q);
+            $url = 'https://www.gsmarena.com/results.php3?sQuickSearch=yes&sName=' . urlencode($q);
             $ngecurl = $this->mycurl($url);
             if ($ngecurl == 'offline') {
                 $result["status"] = "error";
                 $result["data"] = array();
             } else {
-                $html  = str_get_html($ngecurl);
+                $html = str_get_html($ngecurl);
                 $div = $html->find('div[class=makers]', 0);
                 if ($div->find('li', 0)) {
-                    $result["status"] = "sukses";
+                    $result["status"] = 'success';
                     foreach ($div->find('li') as $li) {
                         $grid = $li->find('a', 0);
                         $title = $grid->find('span', 0);
@@ -187,39 +187,38 @@ class Gsm
 
     public function detail($slug = "")
     {
-        
         // Initial ARRAY untuk output
         $result = array();
-            
+
         // Run cURL
-        $url  = 'https://www.gsmarena.com/'.str_replace($this->kata, $this->simbol, $slug).'.php';
+        $url = 'https://www.gsmarena.com/' . str_replace($this->kata, $this->simbol, $slug) . '.php';
         $ngecurl = $this->mycurl($url);
-                
+
         // Jika situs yang di cURL lagi offline/maintenance maka akan menampilkan error message
         if ($ngecurl == 'offline') {
             $result["status"] = "error";
             $result["data"] = array();
         } else {
-            $html  = str_get_html($ngecurl);
+            $html = str_get_html($ngecurl);
             if ($html->find('title', 0)->innertext == '404 Not Found') {
                 $result["status"] = "error";
                 $result["data"] = array();
             } else {
-                $result["status"] = "sukses";
+                $result["status"] = 'success';
                 $result["title"] = $html->find('h1[class=specs-phone-name-title]', 0)->innertext;
-                
+
                 $img_div = $html->find('div[class=specs-photo-main]', 0);
                 $result["img"] = $img_div->find('img', 0)->src;
 
                 // Manipulasi DOM menggunakan library simple html dom. Find div dengan nama class specs-list
                 $div = $html->find('div[id=specs-list]', 0);
-                    
+
                 foreach ($div->find('table') as $table) {
                     $th = $table->find('th', 0);
                     // Membuat array. Find tr from table
                     foreach ($table->find('tr') as $tr) {
                         ($tr->find('td', 0) == "&nbsp;" ? $ttl = "empty" : $ttl = $tr->find('td', 0));
-                        $search  = array(".", ",", "&", "-", " ");
+                        $search = array(".", ",", "&", "-", " ");
                         $replace = array("", "", "", "_", "_");
                         $ttl = strtolower(str_replace($search, $replace, $ttl));
                         $nfo = $tr->find('td', 1);
@@ -228,7 +227,7 @@ class Gsm
                         );
                     }
                 }
-                $search  = array("},{", "[", "]", '","nbsp;":"', "nbsp;", " - ");
+                $search = array("},{", "[", "]", '","nbsp;":"', "nbsp;", " - ");
                 $replace = array(",", "", "", "<br>", "", "<br>- ");
                 $newjson = str_replace($search, $replace, json_encode($result));
                 $result = json_decode($newjson);
